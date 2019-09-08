c4b47878ca309d9c36109ad90143ff2ec409ce85
liztrenholme <32151271+liztrenholme@users.noreply.github.com>
2019-09-08 15:57:14 -0400

    styling, mobile friendly styling

--- src/App.js
+++ src/App.js
@@ -2,10 +2,14 @@ import React from 'react'
 import './App.css'
 import Home from './components/home'
 
+let year = new Date()
+year = year.getFullYear()
+
 function App() {
   return (
     <div className="App">
       <Home />
+      <footer style={{fontSize: '0.8em', alignSelf: 'flex-end'}}>Jackie Pavel {year}</footer>
     </div>
   )
 }
