c71b925d13d6e6fc56e6e02376aafeb3b2cd13ce
liztrenholme <32151271+liztrenholme@users.noreply.github.com>
2019-09-08 15:13:51 -0400

    increased font size, added headshot to about

--- src/components/about/index.js
+++ src/components/about/index.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react'
 import './about.css'
+import headshot from '../../assets/jackie.jpg'
 
 class About extends Component {
   render() {
     return (
       <div className="About">
+        <img className='headshot' src={headshot} alt='Jackie' />
         this will be the About
       </div>
     )
