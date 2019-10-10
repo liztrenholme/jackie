176ccaaac9f5da2be2519c24f6dbd47ed457ae90
liztrenholme <32151271+liztrenholme@users.noreply.github.com>
2019-10-10 09:57:03 -0400

    Updates

---
+++ precache-manifest.88676199bdcf0824f2ffe4994f01982d.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b4d225a1fc006cb9169c0a6e02a9048c",
+    "url": "/jackie/index.html"
+  },
+  {
+    "revision": "3e2a087b2049ee13349a",
+    "url": "/jackie/static/css/main.fd95071e.chunk.css"
+  },
+  {
+    "revision": "ed73935201b715fc140f",
+    "url": "/jackie/static/js/2.b5aafd22.chunk.js"
+  },
+  {
+    "revision": "3e2a087b2049ee13349a",
+    "url": "/jackie/static/js/main.8572201e.chunk.js"
+  },
+  {
+    "revision": "2045e1e0739f30fdea5e",
+    "url": "/jackie/static/js/runtime~main.37319ccc.js"
+  },
+  {
+    "revision": "6e2d46a75fe32a564401c07a6312e024",
+    "url": "/jackie/static/media/couch.6e2d46a7.jpg"
+  },
+  {
+    "revision": "ae54539480c047893948a8a1a1edd9bc",
+    "url": "/jackie/static/media/jackie.ae545394.jpg"
+  },
+  {
+    "revision": "99c4b06a2e83bd7fbece5a1353669a0f",
+    "url": "/jackie/static/media/office1.99c4b06a.jpg"
+  }
+]);
