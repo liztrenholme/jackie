094b7c70e91e9328a5b9bff2c96823a0d7d0edad
liztrenholme <32151271+liztrenholme@users.noreply.github.com>
2019-10-10 09:43:15 -0400

    text for about and mission statement, styling tweaks for contact, pictures added

--- src/components/about/index.js
+++ src/components/about/index.js
@@ -1,25 +1,31 @@
 import React, { Component } from 'react'
 import './about.css'
 import headshot from '../../assets/jackie.jpg'
+import couch from '../../assets/couch.jpg'
 
 class About extends Component {
   render() {
     return (
       <div className="about">
+        <h2>About Me</h2>
         <p className="blurb">
           <img className='headshot' src={headshot} alt='Jackie' />
-        About me about me about me about me about me about me 
-        about me about me about me about me about me about me 
-        about me about me about me about me about me about me 
-        about me about me about me about me about me about me 
-        about me about me about me about me about me about me 
-        about me about me about me about me about me about me 
+          {`I am a graduate of Cleveland State, a CACREP accredited university, and I 
+        hold a Master's degree in education.  I've completed the Clinical Mental 
+        Health counseling program and am a licensed professional counselor.  Clevelend 
+        State's program included many self-growth exercises I swear by because, unless 
+        I understand myself, I cannot possibly help you understand you.  I counsel in all 
+        different issues across the spectrum, and I believe in getting to the root.`}
         </p>
+        <h2>Mission Statement</h2>
         <p className="blurb">
-          Mission statement mission statement mission statement 
-          mission statement mission statement mission statement 
-          mission statement mission statement mission statement 
-          mission statement 
+          <img className='heropic' src={couch} alt='office' />
+          {`It is my mission to take what I have learned and use it to help you 
+          better understand yourself, help you grow in ways that are priceless.  Two 
+          minds are better than one, and two hearts are better than one.  I plan on 
+          being your partner in self-growth, in a loving, caring, challenging and 
+          yet enjoyable and positive environment.  You already have all of your answers, 
+          and I will help you find them.`}
         </p>
       </div>
     )
--- src/components/schedule/index.js
+++ src/components/schedule/index.js
@@ -1,10 +1,16 @@
 import React, { Component } from 'react'
 import './schedule.css'
+import office from '../../assets/office1.jpg'
 
 class Schedule extends Component {
   render() {
     return (
       <div className="schedule">
+        <h2>Schedule an Appointment With Me</h2>
+        <img className='heropic' src={office} alt='office' />
+        schedule blurb schedule blurb schedule blurb schedule blurb 
+        schedule blurb schedule blurb schedule blurb schedule blurb 
+        schedule blurb schedule blurb schedule blurb schedule blurb 
         <p>Calendly plug-in here?</p>
       </div>
     )
